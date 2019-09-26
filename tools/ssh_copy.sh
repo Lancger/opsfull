7cd13556dc231c1502c4a22f41a8b21380ede555
Lancger <1151980610@qq.com>
2019-09-26 16:09:19 +0800

    Update ssh_copy.sh

--- tools/ssh_copy.sh
+++ tools/ssh_copy.sh
@@ -2,7 +2,7 @@
 
 for i in `echo master01 master02 master03 slave01 slave02 slave03`;do
 expect -c "
-spawn scp $1 root@$i:$1
+spawn scp $1 root@$i:$2
     expect {
             \"*yes/no*\" {send \"yes\r\"; exp_continue}
             \"*password*\" {send \"123456\r\"; exp_continue}
