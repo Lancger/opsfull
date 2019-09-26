62720b87abb441e0ac447527aa1ccea7eb9640da
Lancger <1151980610@qq.com>
2019-09-26 15:48:36 +0800

    Create ssh_copy.sh

---
+++ tools/ssh_copy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in `echo master01 master02 master03 slave01 slave02 slave03`;do
+expect -c "
+spawn scp $1 root@$i:$1
+    expect {
+            \"*yes/no*\" {send \"yes\r\"; exp_continue}
+            \"*password*\" {send \"123456\r\"; exp_continue}
+            \"*Password*\" {send \"123456\r\";}
+    } "
+done
